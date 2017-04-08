92dc4a3a69c5b65f5702e1ca8a94a66aaf3a1f98
Ai Sano <sanox013@morris.umn.edu>
2017-04-08 17:40:52 -0500

    Start working on adding the ability to filter by common name
    * common-name.ts: created an empty class
    * common-name-list.service.ts: added a method, "getCommonNames", to get a list of unique common names
    
    Addresses the issue #4

---
+++ client/src/app/garden/common_name_list/src/common-name.ts
@@ -0,0 +1,9 @@
+/**
+ * Empty class for easy-to-read naming conventions for Common Names.
+ * If this is to be removed we should update how the common names are reported from the server.
+ * (Instead of _id:"1S" it should be _id:'unique_id', common_name:"Angelonia")
+ *
+ * @author Iteration 3 - Team revolver en guardia
+ */
+export class CommonName {
+}
