b53305cc568cfc19833e35f428b932b640b0fb8e
Ai Sano <sanox013@morris.umn.edu>
2017-04-08 17:24:25 -0500

    Add a function to test the method, "getCommonNamesAsJson"
    * CommonName.java: created a class, "CommonName", in order to create an array of the class for the testing
    * TestCommonNames.java: the method, "GetCommonNameAsJsonRemoveDuplicates", tests the values returned by "getCommonNamesAsJson"
    * The tests have passed but need to add more
    
    Addresses the issue #4

---
+++ server/src/test/java/umm3601/plant/TestCommonNames.java
@@ -0,0 +1,37 @@
+package umm3601.plant;
+
+import com.google.gson.Gson;
+import org.junit.Before;
+import org.junit.Test;
+import umm3601.digitalDisplayGarden.PlantController;
+
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class TestCommonNames {
+
+    private final static String databaseName = "data-for-testing-only";
+    private PlantController plantController;
+
+    @Before
+    public void populateDB() throws IOException {
+        PopulateMockDatabase db = new PopulateMockDatabase();
+        db.clearAndPopulateDBAgain();
+        plantController = new PlantController(databaseName);
+    }
+
+    @Test
+    public void GetCommonNameAsJsonRemovesDuplicates() throws IOException {
+        CommonName[] commonNames;
+        Gson gson = new Gson();
+
+        String json = plantController.getCommonNamesAsJson("first uploadId");
+        commonNames = gson.fromJson(json, CommonName[].class);
+        assertEquals("Incorrect number of unique common names", 2, commonNames.length);
+        assertEquals("Incorrect zero index", "Alternanthera", commonNames[0]._id);
+        assertEquals("Incorrect value for index 0", "Alternanthera", commonNames[0]._id);
+    }
+
+}
+
