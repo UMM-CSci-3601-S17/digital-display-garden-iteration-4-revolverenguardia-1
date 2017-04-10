6bd5a622b7bc35c891a159d35c70a7757e7d1e1d
Dogxx000 <scot0530@morris.umn.edu>
2017-04-09 21:14:20 -0500

    Done: hours vs page visits
    * Bug: Does not account for data across multiple days
    * TODO: Needs testing

--- server/src/main/java/umm3601/digitalDisplayGarden/GardenCharts.java
+++ server/src/main/java/umm3601/digitalDisplayGarden/GardenCharts.java
@@ -1,20 +1,36 @@
 package umm3601.digitalDisplayGarden;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
-import com.google.gson.annotations.JsonAdapter;
 import com.mongodb.MongoClient;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.*;
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Sorts;
 import com.mongodb.util.JSON;
+import org.bson.BsonInvalidOperationException;
 import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import org.bson.conversions.Bson;
+import org.joda.time.DateTime;
+
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.util.Iterator;
+
+import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.and;
+import static com.mongodb.client.model.Filters.exists;
+import static com.mongodb.client.model.Projections.include;
+import static com.mongodb.client.model.Updates.*;
+import static com.mongodb.client.model.Projections.fields;
 
 import java.io.IOException;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
+import static com.mongodb.client.model.Updates.push;
 /**
  * Created by Dogxx000 on 4/8/17.
  */
@@ -46,10 +62,57 @@ public class GardenCharts
             dataTable[0][0] = "Hour";
             dataTable[0][1] = "Views";
 
+
+            // DB STUFF
+            ArrayList<Date> dates = new ArrayList<Date>();
+
+            //Get a plant by plantID
+            FindIterable doc = plantCollection.find();
+
+            Iterator iterator = doc.iterator();
+            while(iterator.hasNext()) {
+                Document result = (Document) iterator.next();
+
+                //Get metadata.rating array
+                List<Document> ratings = (List<Document>) ((Document) result.get("metadata")).get("visits");
+
+                //Loop through all of the entries within the array, counting like=true(like) and like=false(dislike)
+
+                for(Document i : ratings){
+                    dates.add(((ObjectId) i.get("visit")).getDate());
+                }
+
+            }
+            System.out.println(dates.toString());
+
+            HashMap<Integer, Integer> hours = new HashMap<Integer, Integer>();
+            for (Date date : dates){
+                System.out.print("Date: " + date.toString() + " | ");
+
+                int hour = date.getHours();
+                System.out.print("Hour: " + hour + " | ");
+
+                if(hours.get(hour) == null) hours.put(hour, 0);
+                int visits = hours.get(hour);
+                hours.put(hour, visits += 1);
+                System.out.println(hours.get(hour));
+            }
+
+            System.out.println("hours: " + hours.toString());
+
+            for(int i = 0; i < 25; i++){
+                if(hours.get(i) == null){
+                    hours.put(i, 0);
+                }
+            }
+
+            System.out.println("hours: " + hours.toString());
+            // DB STUFF
+
             Random rand = new Random();
             for (int i = 1; i < 24 + 1; i++) {
                 dataTable[i][0] = Integer.toString(i - 1);
-                dataTable[i][1] = Math.abs(rand.nextInt()) % 300; //TODO: put REAL data here
+                dataTable[i][1] = hours.get(i - 1).intValue(); //TODO: put REAL data here
             }
 
             System.out.println(makeJSON(dataTable));
