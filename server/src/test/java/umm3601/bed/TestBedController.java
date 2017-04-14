9dd1e2193f485b8cb3f40580585370d0f61592e4
carav008 <carav008@morris.umn.edu>
2017-04-13 20:00:01 -0500

    Test for QR scans and visits and test for JsonfeedbackForPlantByPlantId

--- server/src/test/java/umm3601/bed/TestBedController.java
+++ server/src/test/java/umm3601/bed/TestBedController.java
@@ -17,7 +17,6 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 
-import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -43,15 +42,11 @@ public class TestBedController {
         MongoDatabase db = mongoClient.getDatabase(databaseName);
         MongoCollection beds = db.getCollection("beds");
 
-//        String bedJson = "{ \"_id\" : \"ObjectId(\"58edb5ac0b82b43e307ff9f1\")\", \"gardenLocation\" : \"7.0\", \"metadata\" : " +
-//                "{ \"pageViews\" : \"2\", \"visits\" : [ { \"visit\" : \"ObjectId(\"58edb5e90b82b43e307ffa18\")\" }, { \"visit\" : \"ObjectId(\"58edb5ec0b82b43e307ffa1b\")\" } ], \"qrScans\" : [ ] }, \"uploadId\" : \"first uploadId\" }";
-
         FindIterable doc = beds.find(new Document().append("_id", new ObjectId("58d1c36efb0cac4e15afd302")));
         Iterator iterator = doc.iterator();
         Document result = (Document) iterator.next();
 
         int bedPageViews =  (int)((Document) result.get("metadata")).get("pageViews");
-        System.out.println(bedPageViews);
 
         assertEquals("this bed should only have 1 pageView",1, bedPageViews);
 
@@ -87,5 +82,53 @@ public class TestBedController {
 
     }
 
+    @Test
+    public void TestQRScansAndQRVisits(){
+        //first lets test qr scans
+        bedController.incrementBedMetadata("7.0","qrScans","first uploadId");
+        MongoClient mongoClient = new MongoClient();
+        MongoDatabase db = mongoClient.getDatabase(databaseName);
+        MongoCollection beds = db.getCollection("beds");
+
+        FindIterable doc = beds.find(new Document().append("_id", new ObjectId("58d1c36efb0cac4e15afd302")));
+        Iterator iterator = doc.iterator();
+        Document result = (Document) iterator.next();
+
+        int qrScans =  (int)((Document) result.get("metadata")).get("qrScans");
+
+        assertEquals("this bed should have been scanned once ",1, qrScans);
+
+
+        //now we have 1 qrScan for this gardenLocation so the next method that is called should have 2 scans but
+        // report only 1 scan in qrVisits Document
+
+        // make sure that qr bed visits works
+        bedController.addBedQRVisit("7.0","first uploadId");
+
+        FindIterable doc1 = beds.find(new Document().append("_id", new ObjectId("58d1c36efb0cac4e15afd302")));
+        Iterator iterator1 = doc1.iterator();
+        Document result1 = (Document) iterator1.next();
+
+        List<Document> qrVisits =  (List<Document>)((Document) result1.get("metadata")).get("qrVisits");
+        int qrScans2 =  (int) ((Document)result1.get("metadata")).get("qrScans");
+
+        //should also increment the pageViews as it is actually visiting the page
+        int pageViews =  (int) ((Document)result1.get("metadata")).get("pageViews");
+        assertEquals("this should have 1 visit via qr scanning",1 ,qrVisits.size());
+
+        Document qrvisit = qrVisits.get(0);
+        ObjectId objectId = new ObjectId();
+
+        String s = qrvisit.get("scan").toString();
+
+        //after already having 1 qr scan the qr visit should increment qrScans
+        assertEquals("there should be two qr scans now",2,qrScans2);
+        assertEquals("there should be one pageView via the qr scanned ",1,pageViews);
+        //checking to see that the type of visit is an of type/structure of ObjectID
+        assertEquals("they should both be of type org.bson.types.ObjectId ",objectId.getClass().getName(),qrvisit.get("scan").getClass().getName());
+        assertEquals("the object id produced from a visit must be 24 characters",24,s.length());
+
+
+    }
 
 }
---
+++ server/src/test/java/umm3601/plant/TestGetFeedbackCounts.java
@@ -0,0 +1,53 @@
+package umm3601.plant;
+
+import org.junit.Before;
+import org.junit.Test;
+import umm3601.digitalDisplayGarden.PlantController;
+
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Created by carav008 on 4/13/17.
+ */
+public class TestGetFeedbackCounts {
+
+    private final static String databaseName = "data-for-testing-only";
+    private PlantController plantController;
+
+    @Before
+    public void populateDB() throws IOException {
+        PopulateMockDatabase db = new PopulateMockDatabase();
+        db.clearAndPopulateDBAgain();
+        plantController = new PlantController(databaseName);
+    }
+
+
+    @Test
+    public void TestGetJSONFeedbackForPlantByPlantId(){
+
+        //Add flower likes and dislikes to a plant
+        String jsonLike1 = "{like: true, id: \"16001.0\"}";
+        String jsonLike2 = "{like: true, id: \"16001.0\"}";
+        String jsonDislike1= "{like: false, id: \"16001.0\"}";
+        plantController.addFlowerRating(jsonLike1,"first uploadId");
+        plantController.addFlowerRating(jsonLike2,"first uploadId");
+        plantController.addFlowerRating(jsonDislike1,"first uploadId");
+
+        // Add a comment to the comment collection
+        String json = "{ plantId: \"16001.0\", comment : \"Here is our comment for this test\" }";
+        plantController.storePlantComment(json, "first uploadId");
+
+
+        String counts = plantController.getJSONFeedbackForPlantByPlantID("16001.0","first uploadId");
+
+        System.out.println(counts);
+
+        assertEquals("this should be in json format","{ \"likeCount\" : 2 , \"dislikeCount\" : 1 , \"commentCount\" : 1}",counts);
+//        assertEquals("the count should have 2 likes ", "\"likeCount\" : 2",counts.substring(2,17));
+//        assertEquals("the count should have 1 dilikes ", "\"dislikeCount\" : 1",counts.substring(20,38));
+//        assertEquals("the count should have 1 comments ","\"commentCount\" : 1", counts.substring(41,59));
+    }
+
+}
