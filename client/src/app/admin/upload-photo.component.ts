ead3f0a60f821738cdb930ed2033916e27790275
holma198 <holma198@morris.umn.edu>
2017-04-23 17:16:09 -0500

    Add a method of selecting from a list of flowers to create a folder with the appropriate flower name
    To-Do: make buttons prettier, and have a dropdown for beds (perhaps more filters) so the dropdown isn't 400+ flowers long.
    
    issue(s) :
    #2
    #4

--- client/src/app/admin/upload-photo.component.ts
+++ client/src/app/admin/upload-photo.component.ts
@@ -1,22 +1,32 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AdminService } from './admin.service';
 import {ImageUploadComponent} from "./image-upload.component";
-
+import {PlantListComponent} from "../../app/garden/components/plant_list/src/plant-list.component";
+import {Plant} from "../../app/garden/components/plant_list/src/plant";
+import {PlantListService} from "../../app/garden/components/plant_list/src/plant-list.service";
+import { Observable } from "rxjs";
 
 @Component({
     selector: 'upload-photo-component',
     templateUrl: 'upload-photo.component.html',
 })
 
+
 export class UploadPhotoComponent implements OnInit {
+
+    constructor(private plantListService: PlantListService){ }
 
     @ViewChild('iu') iu;
     @ViewChild('nameInput') nameInput: ElementRef;
+
+    ListOfAllPlants: Observable<Plant[]>;
 
     newFileName: string;
     flowerName: string;
     filename:string;
     uploadAttempted:boolean = false;
+    public currentFlower: string;
+    dropDownName:string = "Flower Name";
 
 
     handleUpload() {
@@ -32,6 +42,16 @@ export class UploadPhotoComponent implements OnInit {
 
         );
     }
+
+    setFlower(cultiVar) {
+        this.dropDownName = cultiVar;
+        this.flowerName = cultiVar;
+    }
+
+    retrievePlantNames() {
+        this.ListOfAllPlants = this.plantListService.getPlantsFromServer()
+    }
+
 
     ngOnInit(): void {
 
