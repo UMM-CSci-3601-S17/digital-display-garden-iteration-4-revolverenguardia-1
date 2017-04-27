deb6627c0a83591d2715ea0c9072b9cb298cb54d
hamme503 <hamme503@morris.umn.edu>
2017-04-27 03:05:35 -0500

    Attempted again to fix testStoreImage
    Testing

--- server/src/test/java/umm3601/admin/TestImageUploads.java
+++ server/src/test/java/umm3601/admin/TestImageUploads.java
@@ -58,7 +58,7 @@ public class TestImageUploads {
         }
         handler.storeImage(imageTestDir,testFlowerName,img);
         assertNotNull(testFile.listFiles()[0]);
-        assertEquals(img.toString().substring(23), extImg.toString().substring(21));
+        assertEquals(img.toString().substring(img.toString().length()-(img.toString().length()-30)), extImg.toString().substring(extImg.toString().length()-(img.toString().length()-30)));
     }
 
     /*@Test
